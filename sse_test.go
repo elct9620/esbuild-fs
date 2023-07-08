a9ff5dbf90c57235b864694138f8f97faf676cd1
Aotokitsuruya <contact@aotoki.me>
2023-07-09 00:20:57 +0800

    Refactor SSE test

--- sse_test.go
+++ sse_test.go
@@ -16,34 +16,38 @@ func Test_SSE(t *testing.T) {
 	t.Parallel()
 
 	sse := esbuildfs.NewSSE()
-	server := httptest.NewServer(sse)
-	defer server.Close()
+	serverURL := givenAServer(t, sse)
+	ctx := givenAContextWithTimeout(t, 1*time.Second)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	res := connectSSE(t, ctx, server.URL)
-	events := readEvents(bufio.NewReader(res.Body))
-
-	err := sse.NotifyChanged([]string{"app.js"})
-	if err != nil {
-		t.Fatal("unable to notify changes", err)
-	}
-
-	expected := `{"updated":["app.js"]}`
-	for {
-		select {
-		case <-ctx.Done():
-			t.Fatal("unable to find expected event", expected)
-		case event := <-events:
-			if strings.Contains(event, expected) {
-				return
-			}
-		}
-	}
+	res := whenConnectSSE(t, ctx, serverURL)
+	events := whenReceiveEvents(t, bufio.NewReader(res.Body))
+	whenNotifyChanges(t, sse, []string{"app.js"})
+	thenCanSeeEvent(t, ctx, events, `{"updated":["app.js"]}`)
 }
 
-func connectSSE(t *testing.T, ctx context.Context, url string) *http.Response {
+func givenAServer(t *testing.T, handler http.Handler) string {
+	t.Helper()
+
+	server := httptest.NewServer(handler)
+	t.Cleanup(func() {
+		server.Close()
+	})
+
+	return server.URL
+}
+
+func givenAContextWithTimeout(t *testing.T, duration time.Duration) context.Context {
+	t.Helper()
+
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	t.Cleanup(func() {
+		cancel()
+	})
+
+	return ctx
+}
+
+func whenConnectSSE(t *testing.T, ctx context.Context, url string) *http.Response {
 	t.Helper()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
@@ -59,9 +63,10 @@ func connectSSE(t *testing.T, ctx context.Context, url string) *http.Response {
 	return res
 }
 
-func readEvents(reader *bufio.Reader) chan string {
-	events := make(chan string)
+func whenReceiveEvents(t *testing.T, reader *bufio.Reader) chan string {
+	t.Helper()
 
+	events := make(chan string)
 	go func() {
 		for {
 			next, err := reader.ReadString('\n')
@@ -75,4 +80,28 @@ func readEvents(reader *bufio.Reader) chan string {
 	}()
 
 	return events
+}
+
+func whenNotifyChanges(t *testing.T, notifier esbuildfs.Notifier, changes []string) {
+	t.Helper()
+
+	err := notifier.NotifyChanged(changes)
+	if err != nil {
+		t.Fatal("unable to notify changes", err)
+	}
+}
+
+func thenCanSeeEvent(t *testing.T, ctx context.Context, events chan string, payload string) {
+	t.Helper()
+
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatal("unable to find expected event", payload)
+		case event := <-events:
+			if strings.Contains(event, payload) {
+				return
+			}
+		}
+	}
 }
